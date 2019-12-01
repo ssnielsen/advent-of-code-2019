9c3d67c60b813fa278c605cfef95f70d3cc759c5
ssnielsen <soeren.nielsen@me.com>
2019-12-01 09:39:44 +0100

    Day 1

--- src/01/day.ts
+++ src/01/day.ts
@@ -1,3 +1,34 @@
+import * as R from 'ramda';
+
+import * as Util from '../util';
+
+const inputData = Util.loadInput('01').map(Number);
+
+const calculateFuel = (mass: number) => {
+    return Math.floor(mass / 3) - 2;
+};
+
+const part1 = (input: typeof inputData) => {
+    return R.pipe(R.map(calculateFuel), R.sum)(inputData);
+};
+
+const calculateFuelChain = (mass: number) => {
+    let fuel = calculateFuel(mass);
+    let totalFuel = 0;
+
+    while (fuel > 0) {
+        totalFuel += fuel;
+        fuel = calculateFuel(fuel);
+    }
+
+    return totalFuel;
+};
+
+const part2 = (input: typeof inputData) => {
+    return R.pipe(R.map(calculateFuelChain), R.sum)(inputData);
+};
+
 export const run = () => {
-    console.log('running');
+    console.log('Part 1', part1(inputData));
+    console.log('Part 1', part2(inputData));
 };
