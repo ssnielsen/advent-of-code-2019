1ef8d6c728dcbbc5d86f75c7728fcf5ffa06abdf
ssnielsen <soeren.nielsen@me.com>
2019-12-01 10:10:27 +0100

    Play with Ramda

--- src/01/day.ts
+++ src/01/day.ts
@@ -2,33 +2,42 @@ import * as R from 'ramda';
 
 import * as Util from '../util';
 
-const inputData = Util.loadInput('01').map(Number);
+type Input = number[];
+
+// const calculateFuel = R.pipe(R.flip(R.divide)(3), Math.floor, R.flip(R.subtract)(2));
 
 const calculateFuel = (mass: number) => {
     return Math.floor(mass / 3) - 2;
 };
 
-const part1 = (input: typeof inputData) => {
-    return R.pipe(R.map(calculateFuel), R.sum)(inputData);
+// const calculateFuelChain = (mass: number): number => {
+//     return R.cond<number, number>([
+//         [R.gt(R.__, 0), fuel => fuel + calculateFuelChain(fuel)],
+//         [R.T, R.always(0)],
+//     ])(calculateFuel(mass));
+// };
+
+const calculateFuelChain = (mass: number): number => {
+    const fuel = calculateFuel(mass);
+
+    return fuel > 0 ? fuel + calculateFuelChain(fuel) : 0;
 };
 
-const calculateFuelChain = (mass: number) => {
-    let fuel = calculateFuel(mass);
-    let totalFuel = 0;
-
-    while (fuel > 0) {
-        totalFuel += fuel;
-        fuel = calculateFuel(fuel);
-    }
-
-    return totalFuel;
+const accumulateFuel = (input: Input, method: (mass: number) => number) => {
+    return R.pipe(R.map(method), R.sum)(input);
 };
 
-const part2 = (input: typeof inputData) => {
-    return R.pipe(R.map(calculateFuelChain), R.sum)(inputData);
+const part1 = (input: Input) => {
+    return accumulateFuel(input, calculateFuel);
+};
+
+const part2 = (input: Input) => {
+    return accumulateFuel(input, calculateFuelChain);
 };
 
 export const run = () => {
+    const inputData = Util.loadInput('01').map(Number);
+
     console.log('Part 1', part1(inputData));
     console.log('Part 2', part2(inputData));
 };
