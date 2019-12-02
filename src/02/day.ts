46c43199be8ed132bb74ea0d493bbd23ae55bf20
ssnielsen <soeren.nielsen@me.com>
2019-12-02 18:59:10 +0100

    Day 2 Part 1

--- src/02/day.ts
+++ src/02/day.ts
@@ -1,20 +1,26 @@
 import * as Util from '../util';
 
-enum Op {
-    Add = 1,
-    Mul = 2,
-    Halt = 99,
-}
-
 type Program = number[];
 
+const print = (program: Program) => {
+    console.log(program.join(' '));
+};
+
 const execute = (program: Program, pointer: number = 0): number[] => {
-    switch (program[pointer]) {
+    // print(program);
+    const instruction = program[pointer];
+    const opPointer1 = program[pointer + 1];
+    const opPointer2 = program[pointer + 2];
+    const op1 = program[opPointer1];
+    const op2 = program[opPointer2];
+    const destination = program[pointer + 3];
+
+    switch (instruction) {
         case 1: // Add
-            program[program[pointer + 3]] = program[pointer + 1] + program[pointer + 2];
+            program[destination] = op1 + op2;
             return execute(program, pointer + 4);
         case 2: // Mul
-            program[program[pointer + 3]] = program[pointer + 1] * program[pointer + 2];
+            program[destination] = op1 * op2;
             return execute(program, pointer + 4);
         case 99: // Halt
             return program;
@@ -28,11 +34,15 @@ const part1 = (program: Program) => {
 };
 
 export const run = () => {
-    // const inputData = Util.loadInput('02')[0]
-    //     .split(',')
-    //     .map(Number);
+    const inputData = Util.loadInput('02')[0]
+        .split(',')
+        .map(Number);
 
-    const inputData = '1,9,10,3,2,3,11,0,99,30,40,50'.split(',').map(Number);
+    // // const inputData = '1,9,10,3,2,3,11,0,99,30,40,50'.split(',').map(Number);
+    // const inputData = '2,4,4,5,99,0'.split(',').map(Number);
+
+    inputData[1] = 12;
+    inputData[2] = 2;
 
     console.log('Part 1', part1(inputData));
     console.log('Done');
