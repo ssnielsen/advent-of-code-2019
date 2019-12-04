a7101c9996a42ff39472b4711782e07402ff098f
ssnielsen <soeren.nielsen@me.com>
2019-12-04 19:29:02 +0100

    Day 4 Part 2

--- src/04/day.ts
+++ src/04/day.ts
@@ -1,10 +1,10 @@
 import * as R from 'ramda';
 
-const isRepeating = ([c1, c2]: string[]) => {
+const isRepeating = ([c1, c2]: [string, string]) => {
     return c1 === c2;
 };
 
-const isNotDecreasing = ([c1, c2]: string[]) => {
+const isNotDecreasing = ([c1, c2]: [string, string]) => {
     return c1 <= c2;
 };
 
@@ -17,12 +17,31 @@ const part1 = (from: number, to: number) => {
         () => R.range(from, to + 1),
         R.map(R.pipe(String, R.split(''), R.aperture(2))),
         R.filter(test),
-    )().length;
+    )();
+};
+
+const part2 = (from: number, to: number) => {
+    return R.pipe(
+        // Merge "apertured" array back together
+        R.map((x: string[][]) => [...x.map(R.head), x[x.length - 1][1]]),
+        R.filter(candidate => {
+            return R.any((i: number) => {
+                return (
+                    candidate[i] === candidate[i + 1] &&
+                    (candidate[i - 1] === undefined ||
+                        candidate[i - 1] !== candidate[i]) &&
+                    (candidate[i + 2] === undefined ||
+                        candidate[i + 1] !== candidate[i + 2])
+                );
+            })(R.range(0, candidate.length));
+        }),
+    )(part1(from, to));
 };
 
 export const run = () => {
     const from = 128392;
     const to = 643281;
 
-    console.log('Part 1:', part1(from, to));
+    console.log('Part 1:', part1(from, to).length);
+    console.log('Part 2:', part2(from, to).length);
 };
