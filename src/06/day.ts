550505f5383b6684d0e3a3e8902cd2d107bfb536
ssnielsen <soeren.nielsen@me.com>
2019-12-06 22:21:03 +0100

    Day 6 Part 2

--- src/06/day.ts
+++ src/06/day.ts
@@ -2,11 +2,6 @@ import {Map, List} from 'immutable';
 
 import * as Util from '../util';
 
-interface Pair {
-    orbitter: string;
-    orbitee: string;
-}
-
 type Planets = Map<string, List<string>>;
 
 const countOrbits = (planet: string, planets: Planets): number => {
@@ -21,13 +16,21 @@ const countOrbits = (planet: string, planets: Planets): number => {
     );
 };
 
-const part1 = (system: Pair[]) => {
-    const planets = system.reduce((planets, {orbitee, orbitter}) => {
-        return planets.update(orbitter, orbits =>
-            (orbits ?? List<string>()).push(orbitee),
-        );
-    }, Map<string, List<string>>());
+const orbitPath = (planet: string, planets: Planets): string[] => {
+    const orbits = planets.get(planet, List<string>());
 
+    return [
+        ...orbits.toArray(),
+        ...orbits.reduce((others, orbitee) => {
+            return [...others, ...orbitPath(orbitee, planets)];
+        }, new Array<string>()),
+    ];
+};
+
+const testInput =
+    'COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN';
+
+const part1 = (planets: Planets) => {
     const totalOrbits = planets.reduce((totalOrbits, orbits, planet) => {
         return totalOrbits + countOrbits(planet, planets);
     }, 0);
@@ -35,11 +38,31 @@ const part1 = (system: Pair[]) => {
     return totalOrbits;
 };
 
-export const run = () => {
-    const system = Util.loadInput('06').map(line => {
-        const [orbitee, orbitter] = line.split(')');
-        return {orbitee, orbitter};
-    });
+const part2 = (planets: Planets) => {
+    const pathFromYOU = orbitPath('YOU', planets).reverse();
+    const pathFromSAN = orbitPath('SAN', planets).reverse();
 
-    console.log('Part 1:', part1(system));
+    let i = 0;
+
+    while (pathFromSAN[i] === pathFromYOU[i]) {
+        i++;
+    }
+
+    return pathFromSAN.length - i + (pathFromYOU.length - i);
+};
+
+export const run = () => {
+    const planets = Util.loadInput('06')
+        .map(line => {
+            const [orbitee, orbitter] = line.split(')');
+            return {orbitee, orbitter};
+        })
+        .reduce((planets, {orbitee, orbitter}) => {
+            return planets.update(orbitter, orbits =>
+                (orbits ?? List<string>()).push(orbitee),
+            );
+        }, Map<string, List<string>>());
+
+    console.log('Part 1:', part1(planets));
+    console.log('Part 2:', part2(planets));
 };
