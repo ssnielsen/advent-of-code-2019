3e1d6ad64429929790e87a49851e32a72b59a445
ssnielsen <soeren.nielsen@me.com>
2019-12-08 10:55:18 +0100

    Day 8 Part 2

--- src/08/day.ts
+++ src/08/day.ts
@@ -5,8 +5,6 @@ import * as Util from '../util';
 type Layers = number[][][];
 type Counts = {[index: string]: number};
 
-const X = (counts: Counts) => R.min(counts[0]);
-
 const part1 = (layers: Layers) => {
     const res = R.pipe(
         R.map((layer: Layers[0]) => R.flatten(layer)),
@@ -21,8 +19,29 @@ const part1 = (layers: Layers) => {
     return res[1] * res[2];
 };
 
-const WIDTH = 25;
+const part2 = (layers: Layers) => {
+    const flattened = R.range(0, HEIGHT).map(x => {
+        return R.range(0, WIDTH).map(y => {
+            return R.range(0, layers.length)
+                .reverse()
+                .reduce((value, layer) => {
+                    const layersValue = layers[layer][x][y];
+                    return layersValue === 2 ? value : layersValue;
+                }, 2);
+        });
+    });
+
+    return flattened;
+};
+
+const printImage = (image: Layers[0]) => {
+    image.forEach(x => {
+        console.log(x.map(pixel => (pixel === 0 ? '█' : ' ')).join(''));
+    });
+};
+
 const HEIGHT = 6;
+const WIDTH = 25;
 
 export const run = () => {
     const input = Util.loadInput('08')[0];
@@ -31,8 +50,10 @@ export const run = () => {
         R.split(''),
         R.map(x => Number(x)),
         R.splitEvery(WIDTH * HEIGHT),
-        R.map(R.splitEvery(6)),
+        R.map(R.splitEvery(WIDTH)),
     )(input) as Layers;
 
     console.log('Part 1:', part1(layers));
+    console.log('Part 2:');
+    printImage(part2(layers));
 };
